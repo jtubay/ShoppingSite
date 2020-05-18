d650426261fe263c695fa34768ccf92aef7ae10e
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-18 19:10:57 -0400

    addeding components

---
+++ src/components/Styled.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+text-transform:capitalize;
+font-size:1.4rem;
+background: transparent;
+border:0.05rem solid var(--lightBlue);
+border-color: ${props => 
+    props.cart ? "var(--mainPurple)" : "var(--mainGold)"};
+color:${props => 
+    (props.cart ? "var(--mainPurple)" : "var(--lightGold)")
+
+};
+border-radius:0.5rem;
+padding:0.2rem 0.5rem;
+cursor: pointer;
+margin:0.2rem 0.5rem 0.2rem 0;
+transition:all 0.5s ease-in-out;
+&:hover{
+    background: ${props =>
+    props.cart ? "var(--mainGold)" : "var(--mainWhite)"};
+    color:var(--mainDark);
+}
+&:focus{
+    outline:none;
+}
+`
+export const NavWrapper = styled.nav`
+background:var(--mainDark);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.8rem;
+    text-transform:capitalize;
+}
+`
